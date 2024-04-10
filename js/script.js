b1456e4b716d992bb679674299dd9f69e429e29f
Neith <neithwideman.np@outlook.com>
2024-04-09 23:26:18 -0400

    ..

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Section I: Variables and Data Types
 
-// A.) 
+// A.) Variables
 //  Declare a new constant variable called myName and assign it your name?
 // Declare a variable called favoriteLanguage and assign it the value of Python?
 // Declare a new variable called newFavoriteLanguage and assign it JavaScript;
@@ -13,25 +13,58 @@ let newFavoriteLanguage = "JavaScript";
  favoriteLanguage = newFavoriteLanguage;
  console.log(`Wassap, its ${myName} and  my new favorite lanuage is ${favoriteLanguage}`);
 
+//B.) strings
+// Create a variable called firstVariable
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable? Log it!
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated. ex: Hello, my name is Jean Valjean
 
-// const a = 4;
-// const b = 53;
-// const c = 57;
-// const d = 16;
-// const e = 'Kevin';
+let firstVariable;
 
-// console.log(a __ b);
-// console.log(c __ d);
-// console.log('Name' __ 'Name');
+firstVariable = "Hello World";
+firstVariable = 3;
+secondVariable = firstVariable;
+secondVariable = "Goodbye World"
+
+console.log(firstVariable) // the value is 3
+
+let yourName = "Neith";
+
+console.log(`Hello, my name is ${yourName}`);
+
+// C.) Booleans
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' == 'Name');
 // // FOR THE NEXT TWO, USE ONLY && OR ||
-// console.log(true __ false);
-// console.log(false __ false __ false __ false __ false __ true);
-// console.log(false __ false)
-// console.log(e ___ 'Kevin');
+console.log(true || false);
+console.log(false || false || false || false || false || true);
+console.log(false && false)
+console.log(e == 'Kevin');
 // console.log(a __ b ___ c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
-// console.log(a __ a ___ d); // note: the answer is a simple arithmetic equation, not something "weird"
-// console.log(48 __ '48');
+console.log(a && a || d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(48 !== '48');
 
+// D.) THe Farm
+// Declare a variable animal. Set it to be either "cow" or something else
+// Write code that will print out "mooooo" if the it is equal to cow
+// Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow." Commit
+
+let animal = "cow";
+if (animal === "cow") {
+    console.log("mooooo");
+} else {
+    console.log("Hey! You're not a cow.");
+}
 
 
 // ==============Section II: Loops================
